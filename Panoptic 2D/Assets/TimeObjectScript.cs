cccbe2e37ea046962f374998eaf1ddc9e8a9d072
leonardzz7 <leonard@leooli.dk>
2023-03-27 11:16:09 +0200

    jbhjjhhvvv

--- Panoptic 2D/Assets/TimeObjectScript.cs
+++ Panoptic 2D/Assets/TimeObjectScript.cs
@@ -64,6 +64,7 @@ public class TimeObjectScript : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
         if (collision.tag == "Player" )
         {
             playerDetected = true;
