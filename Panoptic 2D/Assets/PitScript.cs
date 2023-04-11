ce7394dc929ddab382841a2bba2d7e4a164eafbf
leonardzz7 <leonard@leooli.dk>
2023-04-11 14:49:13 +0200

    asdasd

--- Panoptic 2D/Assets/PitScript.cs
+++ Panoptic 2D/Assets/PitScript.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PitScript : MonoBehaviour
 {
+    [SerializeField] GameObject hider;
+    [SerializeField] GameObject SeekerW;
+    [SerializeField] GameObject RestartMenu;
+    bool falling;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,13 +17,22 @@ public class PitScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (falling == true && hider.transform.localScale.x > 0.001f)
+        {
+            hider.transform.localScale = new Vector3(hider.transform.localScale.x - 0.3f * Time.deltaTime, hider.transform.localScale.y - 0.3f * Time.deltaTime, hider.transform.localScale.z);
+        }
+        if (hider.transform.localScale.x < 0.001f)
+        {
+            SeekerW.SetActive(true);
+            RestartMenu.SetActive(true);
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.tag == "Player")
         {
-            Destroy(collision.gameObject.GetComponent<PlayerMovement>())
+            falling = true;
+            Destroy(collision.gameObject.GetComponent<PlayerMovement>());
         }
     }
 }
