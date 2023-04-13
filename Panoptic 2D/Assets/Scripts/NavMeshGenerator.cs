13fc37065c08d18f14fbb8485ecba26e558ef2a5
BigBoomHeadShot <phillipthomsen865@gmail.com>
2023-04-13 13:24:40 +0200

    13-04 remove NavCom

--- Panoptic 2D/Assets/Scripts/NavMeshGenerator.cs
+++ Panoptic 2D/Assets/Scripts/NavMeshGenerator.cs
@@ -9,20 +9,20 @@ using UnityEngine.AI;
 
 public class NavMeshGenerator : MonoBehaviour
 {
-    public NavMeshSurface Surface2D;
+
     
 
     // Start is called before the first frame update
     void Start()
     {
-        Surface2D = GetComponent<NavMeshSurface>();
+        
         StartCoroutine(GenerateNavmesh());
     }
 
     private IEnumerator GenerateNavmesh()
     {
         yield return new WaitForSeconds(1);
-        Surface2D.UpdateNavMesh(Surface2D.navMeshData);
+        
     }
     // Update is called once per frame
     void Update()
